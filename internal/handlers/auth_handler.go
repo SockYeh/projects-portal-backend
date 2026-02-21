7c6fdb38716317fac34ac6a55828777a8905728d
SockYeh <sahranashfaq@gmail.com>
2026-02-21 14:41:35 +0530

    refactor: actually remove redis_service.go properly

--- internal/handlers/auth_handler.go
+++ internal/handlers/auth_handler.go
@@ -2,13 +2,8 @@ package handlers
 
 import (
 	"net/http"
-	"os"
-	"time"
 
-	"github.com/ComputerSocietyVITC/projects-portal-backend/internal/models"
 	"github.com/ComputerSocietyVITC/projects-portal-backend/internal/service"
-	"github.com/golang-jwt/jwt/v5"
-	"github.com/google/uuid"
 	"github.com/labstack/echo/v5"
 	"go.uber.org/zap"
 )
@@ -23,52 +18,13 @@ func NewAuthHandler(service *service.AuthService, logger *zap.Logger) *AuthHandl
 	}
 }
 
-func jwtKeyFunc(token *jwt.Token) (interface{}, error) {
-	return []byte(os.Getenv("JWT_SECRET")), nil
-}
-
-func (h *AuthHandler) createRefreshToken(userid uuid.UUID) (string, error) {
-	var refreshToken = models.UserRefreshToken{UserID: userid.String()}
-
-	claims := &models.JwtUserRefreshToken{
-		UserID: userid.String(),
-		RegisteredClaims: jwt.RegisteredClaims{
-			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Hour * 24 * 7)),
-		},
-	}
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	refreshTok, _ := token.SignedString([]byte(os.Getenv("JWT_SECRET")))
-	refreshToken.RefreshToken = refreshTok
-
-	if err := h.Service.RedisRepo.SaveRefreshToken(&refreshToken); err != nil {
-		return "", err
-	}
-	return refreshTok, nil
-}
-
-func (h *AuthHandler) isValidRefreshToken(refreshToken string) bool {
-	parsedRefreshToken, _ := jwt.ParseWithClaims(refreshToken, &models.JwtUserRefreshToken{}, jwtKeyFunc)
-
-	claim, ok := parsedRefreshToken.Claims.(*models.JwtUserRefreshToken)
-	if !ok || !parsedRefreshToken.Valid {
-		return false
-	}
-
-	userRefreshToken, err := h.Service.RedisRepo.GetRefreshToken(claim.UserID)
-	if err != nil {
-		return false
-	}
-
-	return userRefreshToken.RefreshToken == refreshToken
-}
-
-type LoginBody struct {
+type loginBody struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
 }
 
 func (h *AuthHandler) Login(c *echo.Context) error {
-	var body LoginBody
+	var body loginBody
 	if err := c.Bind(&body); err != nil {
 		return c.JSON(http.StatusBadRequest, map[string]string{"error": "bad body parameters"})
 	}
@@ -78,7 +34,7 @@ func (h *AuthHandler) Login(c *echo.Context) error {
 		return c.JSON(http.StatusUnauthorized, map[string]string{"error": err.Error()})
 	}
 
-	refreshToken, err := h.createRefreshToken(user.ID)
+	refreshToken, err := h.Service.CreateRefreshToken(user.ID)
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, map[string]string{"error": err.Error()})
 	}
--- internal/service/auth_service.go
+++ internal/service/auth_service.go
@@ -8,6 +8,7 @@ import (
 	"github.com/ComputerSocietyVITC/projects-portal-backend/internal/models"
 	"github.com/ComputerSocietyVITC/projects-portal-backend/internal/repository"
 	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -22,6 +23,41 @@ func NewAuthService(r *repository.AuthRepo, redis *repository.RedisRepo) *AuthSe
 
 func jwtKeyFunc(token *jwt.Token) (interface{}, error) {
 	return []byte(os.Getenv("JWT_SECRET")), nil
+}
+
+func (svc *AuthService) CreateRefreshToken(userid uuid.UUID) (string, error) {
+	var refreshToken = models.UserRefreshToken{UserID: userid.String()}
+
+	claims := &models.JwtUserRefreshToken{
+		UserID: userid.String(),
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Hour * 24 * 7)),
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	refreshTok, _ := token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+	refreshToken.RefreshToken = refreshTok
+
+	if err := svc.RedisRepo.SaveRefreshToken(&refreshToken); err != nil {
+		return "", err
+	}
+	return refreshTok, nil
+}
+
+func (svc *AuthService) IsValidRefreshToken(refreshToken string) bool {
+	parsedRefreshToken, _ := jwt.ParseWithClaims(refreshToken, &models.JwtUserRefreshToken{}, jwtKeyFunc)
+
+	claim, ok := parsedRefreshToken.Claims.(*models.JwtUserRefreshToken)
+	if !ok || !parsedRefreshToken.Valid {
+		return false
+	}
+
+	userRefreshToken, err := svc.RedisRepo.GetRefreshToken(claim.UserID)
+	if err != nil {
+		return false
+	}
+
+	return userRefreshToken.RefreshToken == refreshToken
 }
 
 func (svc *AuthService) Login(email, password string) (*models.User, error) {
